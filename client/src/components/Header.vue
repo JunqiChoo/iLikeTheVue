f2908ef2b741cbbcd8762577548974480d9e2792
Jun Qi Choo <junqi.cjq@gmail.com>
2025-06-18 09:43:03 +0800

    added delete button, fixed empty data display (v-if v-else), fixed logo issue

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -3,7 +3,7 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">TO-DO</a>
+  <a class="navbar-brand ms-3" href="http://localhost:5173/">TO-DO</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -1,151 +1,127 @@
 <template>
-    <h1 class="fw-bold text-center">
-        <br />
-        NOT COMPLETED
-    </h1>
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8" >
-            <ClipLoader v-if="loading"/>
-            <table class="table table-light" v-else>
-                <colgroup>
-                    <col style="width: 10%;">
-                    <col style="width: 60%;">
-                    <col style="width: 10%;">
-                    <col style="width: 10%;">
-                    <col style="width: 10%;">
-                </colgroup>
-                <thead>
-                    <tr>
-                        <th scope="col">No.</th>
-                        <th scope="col">Task Description</th>
-                        <th scope="col">Completed</th>
-                        <th scope="col">Edit</th>
-                        <th scope="col">Delete</th>
-                    </tr>
-                </thead>
-                <tbody v-for="(task,index) in tasks.filter(t => !t.completion_status)" v-bind:key="task.id">
+  <h1 class="fw-bold text-center"><br />NOT COMPLETED</h1>
+  <div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+      
+      <ClipLoader v-if="loading" />
 
+      <!-- NOT COMPLETED TASKS TABLE -->
+      <template v-else>
+        <table v-if="pendingTasks.length > 0" class="table table-light">
+          <colgroup>
+            <col style="width: 10%;" />
+            <col style="width: 60%;" />
+            <col style="width: 10%;" />
+            <col style="width: 10%;" />
+            <col style="width: 10%;" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Task Description</th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(task, index) in pendingTasks" :key="task._id">
+              <th scope="row">{{ index + 1 }}</th>
+              <td>{{ task.task_message }}</td>
+              <td><button @click="completeTask(task._id)" class="btn btn-success btn-sm">Completed</button></td>
+              <td><router-link :to="{ name: 'editTask', params: { id: task._id } }" class="btn btn-primary btn-sm">Edit</router-link></td>
+              <td><button @click="delTask(task._id)" class="btn btn-danger btn-sm">Delete</button></td>
+            </tr>
+          </tbody>
+        </table>
+        <p v-else class="text-center">Sorry! No pending task for now!</p>
 
-                    <tr>
-                        <th scope="row">{{ index + 1 }}</th>
-                        <td>{{task.task_message}}</td>
-                        <td>
-                            <button  @click="completeTask(task._id)"  type="button" class="btn btn-success btn-sm">Completed</button>
-                        </td>
-                       
-                        <td>
-                           <router-link :to="{ name: 'editTask', params: { id: task._id } }" type="button" class="btn btn-primary btn-sm">Edit</router-link>
-                        </td>
-                        <td>
-                            <button @click="delTask(task._id)" type="button" class="btn btn-danger btn-sm">Delete</button>
-                        </td>
-                    </tr>
+        <h1 class="fw-bold text-center"><br />COMPLETED</h1>
 
-                </tbody>
-            </table>
+        <!-- COMPLETED TASKS TABLE -->
+        <table v-if="completedTasks.length > 0" class="table table-light">
+          <colgroup>
+            <col style="width: 10%;" />
+            <col style="width: 80%;" />
+            <col style="width: 10%;" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Task Description</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(task, index) in completedTasks" :key="task._id">
+              <th scope="row">{{ index + 1 }}</th>
+              <td>{{ task.task_message }}</td>
+              <td><button @click="delTask(task._id)" class="btn btn-danger btn-sm">Delete</button></td>
+            </tr>
+          </tbody>
+        </table>
+        <p v-else class="text-center">No completed task!</p>
 
-            <h1 class="fw-bold text-center">
-                <br />
-                COMPLETED
-            </h1>
-            <ClipLoader v-if="loading"/>
-             <table class="table table-light"v-else>
-                <colgroup>
-                    <col style="width: 10%;">
-                    <col style="width: 90%;">
-                  
-                </colgroup>
-                <thead>
-                    <tr>
-                        <th scope="col">No.</th>
-                        <th scope="col">Task Description</th>
-                    </tr>
-                </thead>
-                <tbody v-for="(task,index) in tasks.filter(t => t.completion_status)" v-bind:key="task.id">
-
-
-                    <tr>
-                        <th scope="row">{{ index + 1 }}</th>
-                        <td>{{task.task_message}}</td>
-                    
-                    </tr>
-
-                </tbody>
-            </table>
-
-
-            <div class="text-center">
-                {{ tasks.filter(t => t.completion_status).length }}/{{ tasks.length }} task completed
-            </div>
+        <!-- Summary -->
+        <div class="text-center">
+          {{ completedTasks.length }}/{{ tasks.length }} task(s) completed
         </div>
-        <div class="col-md-2"></div>
+      </template>
+
     </div>
-
+    <div class="col-md-2"></div>
+  </div>
 </template>
-
 
 <script setup>
 import axios from 'axios'
-import { ref, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
 import { useToast } from 'vue-toastification'
 import { useRouter, useRoute } from 'vue-router'
+
 const router = useRouter()
-const route = useRoute();
-const toast = useToast();
+const route = useRoute()
+const toast = useToast()
+
 const tasks = ref([])
+const loading = ref(true)
 const apilink = "http://localhost:3000/api/tasks"
 
+const pendingTasks = computed(() => tasks.value.filter(t => !t.completion_status))
+const completedTasks = computed(() => tasks.value.filter(t => t.completion_status))
 
 const completeTask = async (id) => {
-    const link = `${apilink}/${id}`
-  
-    try {
-        const tempData = await axios.put(link, {completion_status : true})
-        console.log(tempData.data)
-        toast.success("Goodjob On completing it!")
-        getTasks();
-    } catch (error) {
-        toast.error("There seem to be a problem with completing the task")
-        console.log(error);
-    }
-
+  try {
+    await axios.put(`${apilink}/${id}`, { completion_status: true })
+    toast.success("Good job on completing it!")
+    getTasks()
+  } catch (error) {
+    toast.error("There was a problem completing the task")
+    console.error(error)
+  }
 }
 
-
-
-
-const delTask = async(id)=>{
-    try{
-        const link = `${apilink}/${id}`
-        console.log(apilink)
-        const tempData = await axios.delete(link)
-        toast.success("successfully deleted the task")
-        getTasks();
-    }catch(err){
-        console.log(err)
-        toast.error("Unable to delete the task specified")
-    }
+const delTask = async (id) => {
+  try {
+    await axios.delete(`${apilink}/${id}`)
+    toast.success("Successfully deleted the task")
+    getTasks()
+  } catch (err) {
+    toast.error("Unable to delete the task")
+    console.error(err)
+  }
 }
 
 const getTasks = async () => {
   try {
-    const tempData = await axios.get(apilink)
-    console.log("Fetched tasks:", tempData.data)
-    tasks.value = tempData.data
+    const res = await axios.get(apilink)
+    tasks.value = res.data
   } catch (err) {
-    console.error("API fetch failed:", err.message)
-    if (err.response) {
-      console.log("Response status:", err.response.status)
-      console.log("Response data:", err.response.data)
-    } else if (err.request) {
-      console.log("No response received:", err.request)
-    } else {
-      console.log("Setup error:", err.message)
-    }
-  }finally{
-    loading.value = false;
+    console.error("API fetch failed:", err)
+  } finally {
+    loading.value = false
   }
 }
 
